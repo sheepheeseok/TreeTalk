21ea9f827cb6459e5d2183f773f904b733b74e18
heeseok <heeseokid@g.shingu.ac.kr>
2025-02-25 09:58:03 +0900

    Home 화면 제작중

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 const Home = () => {
     return (
-        <div className="container">d</div>
+        <div className="home-container">
+            <div className="icon-container">
+                <img src="/icons/menu.svg" alt="menu" className="menu-icon"/>
+                <img src="/icons/profile.svg" alt="profile" className="profile-icon"/>
+            </div>
+            <h1>TREE TALK</h1>
+        </div>
     )
 }
 
